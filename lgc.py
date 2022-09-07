105d21a5c6e830d8081c49755d787f524b2c2870
emilio2399 <emilio_ca2399@hotmail.com>
2022-09-07 12:22:09 -0500

    numeros aleatorios - lgc

---
+++ lgc.py
@@ -0,0 +1,17 @@
+
+import numpy as np
+
+def LGC (x,a,c,m,corridas):
+    random_list = np.zeros(corridas)
+    for i in range(corridas):
+        subtotal= x*a+c
+        pseudoaleatorio= subtotal % m
+        random= pseudoaleatorio/ m
+        random_list[i]=random
+        x=pseudoaleatorio
+    return random_list
+
+
+x = LGC(6,32,3,80,10)
+print(x)
+
